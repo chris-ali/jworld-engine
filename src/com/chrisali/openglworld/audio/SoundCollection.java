e71885a3f3175c8a35ecf414954cb56ddc9a2afb
Christopher Ali <christopher.ali@outlook.com>
2016-06-10 13:01:01 -0400

    Other Sounds
    - Added other sounds (wind, stall, gear etc) to SoundCollection, created
    methods to play them back, and added javadoc comments

--- src/com/chrisali/openglworld/audio/SoundCollection.java
+++ src/com/chrisali/openglworld/audio/SoundCollection.java
@@ -5,6 +5,13 @@ import java.util.Map;
 
 import org.lwjgl.util.vector.Vector3f;
 
+/**
+ * Static class that contains a repository of sounds to be played by triggering certain events, such
+ * as control surface deflections, engine properties or change in airspeed
+ * 
+ * @author Christopher Ali
+ *
+ */
 public class SoundCollection {
 	public static enum SoundEvent {
 		BOUNCE,
@@ -13,11 +20,21 @@ public class SoundCollection {
 		ENGINE_1_LOW,
 		ENGINE_1_MED,
 		ENGINE_1_HIGH,
-		ENGINE_1_MAX;
+		ENGINE_1_MAX,
+		FLAPS,
+		GEAR,
+		STALL,
+		WIND,
+		GYRO;
 	}
 	
 	private static Map<SoundEvent, SoundSource> soundSources = new EnumMap<>(SoundEvent.class);
+	private static float previousControlValue = 0.0f;
 	
+	/**
+	 *	Fills soundSources EnumMap with {@link SoundSource} objects, which are references to audio
+	 *  files in .Resources/Audio, and sets their initial properties
+	 */
 	public static void initializeSounds() {
 		soundSources.put(SoundEvent.BOUNCE_LOOP, new SoundSource("Audio", "bounce"));
 		soundSources.get(SoundEvent.BOUNCE_LOOP).setLooping(true);
@@ -26,6 +43,8 @@ public class SoundCollection {
 		
 		soundSources.put(SoundEvent.BOUNCE_HIGH, new SoundSource("Audio", "bounce"));
 		soundSources.get(SoundEvent.BOUNCE_HIGH).setPitch(2);
+		
+		//================================ Engine =========================================
 		
 		soundSources.put(SoundEvent.ENGINE_1_LOW, new SoundSource("Audio", "engineLow"));
 		soundSources.get(SoundEvent.ENGINE_1_LOW).setVolume(0);
@@ -43,17 +62,74 @@ public class SoundCollection {
 		soundSources.get(SoundEvent.ENGINE_1_MAX).setVolume(0);
 		soundSources.get(SoundEvent.ENGINE_1_MAX).setLooping(true);
 		
+		//================================ Systems =========================================
+		
+		soundSources.put(SoundEvent.FLAPS, new SoundSource("Audio", "flap"));
+		soundSources.get(SoundEvent.FLAPS).setVolume(0.25f);
+		
+		soundSources.put(SoundEvent.GEAR, new SoundSource("Audio", "gear"));
+		soundSources.get(SoundEvent.GEAR).setVolume(0.125f);
+		
+		soundSources.put(SoundEvent.STALL, new SoundSource("Audio", "stall"));
+		soundSources.get(SoundEvent.STALL).setVolume(0.5f);
+		soundSources.get(SoundEvent.STALL).setLooping(true);
+		
+		soundSources.put(SoundEvent.GYRO, new SoundSource("Audio", "gyroLoop"));
+		soundSources.get(SoundEvent.GYRO).setVolume(0.25f);
+		soundSources.get(SoundEvent.GYRO).setLooping(true);
+		
+		//================================ Environment ======================================
+		
+		soundSources.put(SoundEvent.WIND, new SoundSource("Audio", "wind"));
+		soundSources.get(SoundEvent.WIND).setVolume(0.25f);
+		soundSources.get(SoundEvent.WIND).setLooping(true);
+		
 		// Add new sounds here; can set looping/position properties etc
 	}
 	
-	public static void play(SoundEvent event) {
-		soundSources.get(event).play();
+	/**
+	 * Sounds the stall warning if angle of attack passes a specified threshold (radians)
+	 * 
+	 * @param alpha
+	 * @param threshold
+	 */
+	public static void setStallHorn(float alpha, float threshold) {
+		if ((alpha > threshold) && !(soundSources.get(SoundEvent.STALL).isPlaying()))
+			soundSources.get(SoundEvent.STALL).play();
+		else if ((alpha < threshold) && (soundSources.get(SoundEvent.STALL).isPlaying()))
+			soundSources.get(SoundEvent.STALL).stop();
 	}
 	
-	public static void stop(SoundEvent event) {
-		soundSources.get(event).stop();
+	/**
+	 * Plays sound for a specified control deflection if the difference between the received value (control)
+	 * and the previously received value is greater than 0, indicating a change in deflection over the integration step
+	 * 
+	 * @param control
+	 * @param event
+	 */
+	public static void setControl(float control, SoundEvent event) {
+		if ((Math.abs(control-previousControlValue) > 0) && !soundSources.get(event).isPlaying())
+			soundSources.get(event).play();
+		
+		previousControlValue = control;
 	}
 	
+	/**
+	 * Sets volume of wind as a function of true airspeed (kts)
+	 * 
+	 * @param trueAirspeed
+	 */
+	public static void setWind(float trueAirspeed) {
+		float gainWind = (float) ((trueAirspeed >  50 && trueAirspeed < 300) ? ((2.0-0.25)*(trueAirspeed-50))/(300-50) + 0.25 : 0);
+		soundSources.get(SoundEvent.WIND).setVolume(gainWind);
+	}
+	
+	/**
+	 * Uses sound blending with cosine and linear functions with volume and pitch properties, respectively 
+	 * to mesh together engine sounds as a function of RPM
+	 * 
+	 * @param RPM
+	 */
 	public static void setRPM(float RPM) {
 		float gainLow  = (float) ((RPM >  300 && RPM < 1800) ? Math.cos((RPM-600)/500) : 0);
 		float pitchLow = (float) ((RPM >  300 && RPM < 1800) ? ((1.5-0.75)*(RPM-300))/(1800-300) + 0.75 : 0);
@@ -74,6 +150,14 @@ public class SoundCollection {
 		float pitchMax = (float) ((RPM > 1900 && RPM < 3000) ? ((1.25-0.95)*(RPM-1900))/(3000-1900) + 0.95 : 0);
 		soundSources.get(SoundEvent.ENGINE_1_MAX).setVolume(gainMax);
 		soundSources.get(SoundEvent.ENGINE_1_MAX).setPitch(pitchMax);
+	}
+
+	public static void play(SoundEvent event) {
+		soundSources.get(event).play();
+	}
+	
+	public static void stop(SoundEvent event) {
+		soundSources.get(event).stop();
 	}
 	
 	public static void setPitch(SoundEvent event, float pitch) {
